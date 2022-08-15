8030684494cd91d1b8d31b1e545a79728ff03d0d
Adrian Amir <aami0016@student.monash.edu>
2022-08-15 15:46:01 +0800

    SAFE PUSH

---
+++ prediction_model/choice_model.py
@@ -0,0 +1,13 @@
+import numpy as np
+import os
+import pandas as pd
+import csv
+import re
+import nltk
+from sklearn.datasets import load_files
+nltk.download('stopwords')
+import pickle
+from nltk.corpus import stopwords
+
+
+
