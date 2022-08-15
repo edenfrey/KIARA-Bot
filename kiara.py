09e5d2db80ec7bc002f57c89e1cf6ff6263f062b
Adrian Amir <aami0016@student.monash.edu>
2022-08-15 18:24:20 +0800

    INIT BOT FILE & Classifier Class

---
+++ kiara.py
@@ -0,0 +1,6 @@
+from lib2to3.pgen2 import token
+import hikari
+
+kiara = hikari.GatewayBot(token="MTAwODY3NDA5ODM2NjM4MjEwMA.GUdSAB.GV8EO2oG6lhoC1lZZk9xDRBPiC3qt6aOvwrkFE")
+
+kiara.run()
---
+++ prediction_model/TextClassifier.py
@@ -0,0 +1,3 @@
+class TextClassifier:
+    pass
+    # TODO Create a class object of the classifier that uses the created model to predict.
