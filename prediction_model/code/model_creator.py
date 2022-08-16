7628b6776c2056cddf8e2ca3eacd6fac90ebe77b
Adrian Amir <aami0016@student.monash.edu>
2022-08-16 20:10:15 +0800

    CONTINUED CLASSIFICATION MODEL Created base model and basic <keras> model.
    Needs more tuning and data.

--- prediction_model/code/model_creator.py
+++ prediction_model/code/model_creator.py
@@ -1,13 +1,101 @@
 # Import Libraries
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+
+    # Keras Modeling
+from keras.models import Sequential
+from keras import layers
+from keras.backend import clear_session
+from keras.preprocessing.text import Tokenizer
+from keras_preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras import layers
+
 
 # Importing Data Set
 df = pd.read_csv('prediction_model/datasets/data.csv')
 
-# Text Processing
+# Converting labeled data to targets
+lb_make = LabelEncoder()
+df["target"] = lb_make.fit_transform(df["label"])
 
-# Text Vectorisation
+# Baseline Model
+sentences = df['sentence'].values
+y = df['target'].values
 
-# Model Classification
+sentences_train, sentences_test, y_train, y_test = train_test_split(sentences, y, test_size=0.20, random_state=1000) # Split Data
 
-# Saving of Model
+    # Vectorization of X data
+vectorizer = CountVectorizer() 
+vectorizer.fit(sentences_train)
+X_train = vectorizer.transform(sentences_train)
+X_test  = vectorizer.transform(sentences_test)
+
+    # Creating Model
+classifier = LogisticRegression()
+classifier.fit(X_train, y_train)
+
+    # Testing Model
+score = classifier.score(X_test, y_test)
+print("Accuracy:", score)
+
+# Keras Model
+input_dim = X_train.shape[1]  # Number of features
+
+model = Sequential()
+model.add(layers.Dense(10, input_dim=input_dim, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+clear_session() # Clear Previous Session
+history = model.fit(X_train, y_train,epochs=100,verbose=False,validation_data=(X_test, y_test),batch_size=10)
+
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+
+# Text Pre-Processing
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(sentences_train)
+
+X_train = tokenizer.texts_to_sequences(sentences_train)
+X_test = tokenizer.texts_to_sequences(sentences_test)
+
+vocab_size = len(tokenizer.word_index) + 1  # Adding 1 because of reserved 0 index
+
+maxlen = 100
+
+X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
+X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
+
+# Modelling
+embedding_dim = 50
+
+model = Sequential()
+model.add(layers.Embedding(input_dim=vocab_size, 
+                           output_dim=embedding_dim, 
+                           input_length=maxlen))
+model.add(layers.GlobalMaxPool1D())
+model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+# Testing
+history = model.fit(X_train, y_train,
+                    epochs=50,
+                    verbose=False,
+                    validation_data=(X_test, y_test),
+                    batch_size=10)
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
